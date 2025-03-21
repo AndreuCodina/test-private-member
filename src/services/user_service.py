3427f4de11783af142e004019e2ef146b30a3bb1
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2025-03-21 11:47:52 +0100

    Initial commit

---
+++ src/services/user_service.py
@@ -0,0 +1,22 @@
+class UserService:
+    def sign_up(self, email: str) -> None:
+        if not self._is_valid_email(email):
+            error_message = "Invalid email"
+            raise ValueError(error_message)
+
+        if self._exists(email):
+            error_message = "User already exists"
+            raise ValueError(error_message)
+
+        if not self._associate_bank_account():
+            error_message = "Not allowed to create account"
+            raise ValueError(error_message)
+
+    def _is_valid_email(self, email: str) -> bool:
+        return "@" in email and "." in email
+
+    def _exists(self, email: str) -> bool:
+        return email != "foo@gmail.com"
+
+    def _associate_bank_account(self) -> None:
+        pass
---
+++ tests/services/test_user_service.py
@@ -0,0 +1,16 @@
+from pytest_mock import MockerFixture
+
+from services.user_service import UserService
+
+
+class TestUserService:
+    def test_associate_bank_account(self, mocker: MockerFixture) -> None:
+        user_service = UserService()
+
+        associate_bank_account_mock = mocker.patch(
+            f"{UserService.__module__}.{UserService._associate_bank_account.__qualname__}",
+        )
+
+        user_service.sign_up("fran@gmail.com")
+
+        associate_bank_account_mock.assert_called_once_with()
