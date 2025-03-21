62695dbd89b367c55d7111064825bbac41943fc1
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2025-03-21 12:16:34 +0100

    Commit

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -8,9 +8,7 @@ class UserService:
             error_message = "User already exists"
             raise ValueError(error_message)
 
-        if not self._associate_bank_account():
-            error_message = "Not allowed to create account"
-            raise ValueError(error_message)
+        self._associate_bank_account()
 
     def _is_valid_email(self, email: str) -> bool:
         return "@" in email and "." in email
