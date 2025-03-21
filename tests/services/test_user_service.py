387d2d11b87de8fffaa33935cbeabba4fe273ff0
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2025-03-21 12:44:51 +0100

    Commit

--- tests/services/test_user_service.py
+++ tests/services/test_user_service.py
@@ -7,10 +7,21 @@ class TestUserService:
     def test_associate_bank_account(self, mocker: MockerFixture) -> None:
         user_service = UserService()
 
-        associate_bank_account_mock = mocker.patch(
-            f"{UserService.__module__}.{UserService._associate_bank_account.__qualname__}",
+        # Option 1
+        associate_bank_account_mock = mocker.create_autospec(
+            user_service._associate_bank_account
         )
+        user_service._associate_bank_account = associate_bank_account_mock
+
+        # Option 2
+        # associate_bank_account_mock = mocker.MagicMock()
+        # user_service._associate_bank_account = associate_bank_account_mock
+
+        # Option 3
+        # associate_bank_account_mock = mocker.patch(
+        #     f"{UserService.__module__}.{UserService._associate_bank_account.__qualname__}",
+        # )
 
         user_service.sign_up("fran@gmail.com")
 
-        associate_bank_account_mock.assert_called_once_with()
+        associate_bank_account_mock.assert_called_once()
